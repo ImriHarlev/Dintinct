722919b74878ea94c9e9838634eaac8a76ab58df
imrih <imrih@infraedge.co.il>
2026-04-06 16:24:02 +0300

    Remove NetworkA.Callback.Receiver project

--- src/NetworkA/NetworkA.Callback.Receiver/Interfaces/ICallbackService.cs
+++
@@ -1,9 +0,0 @@
-using Shared.Contracts.Payloads;
-
-namespace NetworkA.Callback.Receiver.Interfaces;
-
-public interface ICallbackService
-{
-    Task HandleFinalStatusAsync(StatusCallbackPayload payload, CancellationToken ct = default);
-    Task HandleChunkRetryRequestAsync(string origJobId, string chunkName, CancellationToken ct = default);
-}
